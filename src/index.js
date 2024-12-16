96d7808f8d60a5265ea10fba691dacad996ff1ee
Florian <floriangeorges1992@gmail.com>
2024-12-16 10:07:27 +0100

    add toISOString - toUTCString

--- src/index.js
+++ src/index.js
@@ -9,4 +9,7 @@
 // console.log(date2);
 
 //format ISO
-const today = new Date();
+const today = new Date("2020-01-10T00:00:00.000Z");
+console.log(today);
+console.log(today.toISOString());
+console.log(today.toUTCString());
