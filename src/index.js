96975d821d8181df6a3d39c0260701d8c6d0905c
Florian <floriangeorges1992@gmail.com>
2024-12-16 10:00:48 +0100

    add declaration date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+//déclarer une date
+// const today = new Date();
+// console.log(today);
+
+// const date = new Date(1000 * 3600 * 24 * 365);
+// console.log(date);
+
+// const date2 = new Date(2020, 0, 10, 50, 80, 25);
+// console.log(date2);
+
+//format ISO
+const today = new Date();
